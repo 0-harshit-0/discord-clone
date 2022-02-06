a3d5310e21116a54a0c1d3d0ce1ddb70c01c4cbb
harshit singhal <harshitw3b@gmail.com>
2022-02-06 17:06:14 +0530

    setting up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <h1>hello world!</h1>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
